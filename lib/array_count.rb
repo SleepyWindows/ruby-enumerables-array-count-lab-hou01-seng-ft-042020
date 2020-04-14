e4c807e4f28d00e29cb65e43a3afba0a026d0587
edwin calvillo <calvillo.edwin@gmail.com>
2020-04-14 04:42:22 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -5,11 +5,7 @@ def count_strings(array)
 end
 
 def count_empty_strings(array)
-  i = 0
-  if array[i].length < 1
     array.count do |element|
-    element = ""
-    end
-    i += 1
+      element = ""
   end
 end
